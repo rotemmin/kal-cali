46b5cfeefb2a8b58378e96f25369ff862db94ae5
rotemmin <rotemminai@gmail.com>
2024-12-18 19:39:23 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import "@/styles/global.css";
+
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+import Navbar from "@/lib/components/Navbar";
+import Footer from "@/lib/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Digital Product Jam Starter Kit",
+  description:
+    "A starter kit for wiritng code in the Digital Product Jam course.",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <head>
+        {/* Browser Favicon */}
+        <link rel="icon" href="/icons/favicon.png" />
+        {/* Apple Icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/icon-180.png"
+        />
+        {/* Android Icon */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/icon-192.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
+      <body>
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
