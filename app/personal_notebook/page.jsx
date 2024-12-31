2a766931a4a0973218a6e79257e0f765e4254345
rotemmin <rotemminai@gmail.com>
2024-12-31 18:50:35 +0200

    מחברת אישית+מדבקות

---
+++ app/personal_notebook/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PersonalNotebookPage = () => {
+  return (
+    <div>
+      <h1>המחברת האישית</h1>
+    </div>
+  );
+};
+
+export default PersonalNotebookPage;
---
+++ app/stickers_notebook/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StickersNotebookPage: React.FC = () => {
+  return (
+    <div>
+      <h1>מחברת המדבקות</h1>
+    </div>
+  );
+};
+
+export default StickersNotebookPage;
