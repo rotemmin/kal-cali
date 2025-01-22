f6b176dfb1592040550b35598326174a8d26c923
Avinoam Nukrai <nukrai.avinoam@mail.huji.ac.il>
2025-01-22 10:41:03 +0200

    last signup

--- lib/components/NavigationButton.jsx
+++ lib/components/NavigationButton.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 import { useRouter } from "next/navigation";
 
 const NavigationButton = ({ label, link, position }) => {
   const router = useRouter();
 
   const buttonStyle = {
-    position: 'fixed',
-    bottom: '20px',
-    [position]: '20px',
-    padding: '10px 20px',
-    backgroundColor: '#0070f3',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
+    position: "fixed",
+    bottom: "20px",
+    [position]: "20px",
+    padding: "10px 20px",
+    backgroundColor: "#0070f3",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
   };
 
   return (
