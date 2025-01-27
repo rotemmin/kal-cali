91777d867a3e8fb982214739ebbc334f97a4f39e
Avinoam Nukrai <nukrai.avinoam@mail.huji.ac.il>
2025-01-27 21:07:50 +0200

    changing the app logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,25 +15,23 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="he" dir="rtl">
       <head>
         {/* Browser Favicon */}
-        <link rel="icon" href="/icons/favicon.png" />
+        <link rel="icon" href="/icons/app_logo.svg" />
         {/* Apple Icon */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/icon-180.png"
+          href="/icons/app_logo.svg"
         />
         {/* Android Icon */}
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/icons/icon-192.png"
+          href="/icons/app_logo.svg"
         />
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
