d7ff90f5a3a291704cec514a6f1ca30d1f4d3b64
saradauksz <sara.dauksz@mail.huji.ac.il>
2025-01-28 12:10:09 +0200

    homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Navbar />
-          {children}
-          <Footer />
-        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </body>
     </html>
   );
