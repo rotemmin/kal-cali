ac5d6d56ea8f12d7a189cea1899a55880bb3534e
rotemmin <rotemminai@gmail.com>
2025-06-18 21:46:38 +0300

    under 700 px height

--- src/components/signup/LoginLink.tsx
+++ src/components/signup/LoginLink.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import { useSignup } from '@/context/SignupContext';
 import styles from './page.module.css';
 
 export default function LoginLink() {
+  const { showEmailForm } = useSignup();
+  
   return (
-    <p className={`register-question ${styles.registerQuestion}`}>
+    <p className={`register-question ${styles.registerQuestion} ${showEmailForm ? styles.emailForm : ''}`}>
       כבר יש לך חשבון?{' '}
       <Link href="/login" className={styles.registerLink}>
         התחבר.י כאן!
