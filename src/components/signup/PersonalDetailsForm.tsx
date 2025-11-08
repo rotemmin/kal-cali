e9be0e2c71ac3850e5a32a58c8b4c6a369e71444
rotemmin <rotemminai@gmail.com>
2025-11-09 00:47:40 +0200

    wait

--- src/components/signup/PersonalDetailsForm.tsx
+++ src/components/signup/PersonalDetailsForm.tsx
@@ -68,7 +68,7 @@ export default function PersonalDetailsForm() {
         className={styles.personalSubmitButton} 
         disabled={loading || !isFormValid}
       >
-        {loading ? 'המתן...' : 'הרשמה'}
+        {loading ? 'המתינו...' : 'הרשמה'}
       </button>
 
       <LoginLink variant="personalDetails" />
